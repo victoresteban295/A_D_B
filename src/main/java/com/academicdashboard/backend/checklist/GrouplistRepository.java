c6c49779aa9e6c43f202f14634367d87f538224c
victoresteban295 <esteban.victor295@gmail.com>
2023-06-01 23:33:57 -0700

    More Test Cases for ChecklistService

--- src/main/java/com/academicdashboard/backend/checklist/GrouplistRepository.java
+++ src/main/java/com/academicdashboard/backend/checklist/GrouplistRepository.java
@@ -1,17 +1,10 @@
 package com.academicdashboard.backend.checklist;
 
-import java.util.Optional;
-
 import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.DeleteQuery;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GrouplistRepository extends MongoRepository<Grouplist, ObjectId> {
 
-    Optional<Grouplist> findGrouplistByListId(String groupId);
-   
-    @DeleteQuery
-    void deleteGrouplistByGroupId(String groupId);
 }
--- src/test/java/com/academicdashboard/backend/checklist/ChecklistServiceTest.java
+++ src/test/java/com/academicdashboard/backend/checklist/ChecklistServiceTest.java
@@ -1,7 +1,6 @@
 package com.academicdashboard.backend.checklist;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,8 +13,7 @@ import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
-
-import com.academicdashboard.backend.student.Student;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class ChecklistServiceTest {
@@ -42,12 +40,11 @@ public class ChecklistServiceTest {
     @Test
     @DisplayName("Should Create a Ungrouped Checklist")
     @SuppressWarnings("unchecked")
-    @Disabled
     public void canCreateNewChecklist() {
         Checklist expectedValue = new Checklist("testId", "Title");
 
         //Given 
-        String userId = "testId";
+        String userId = "userId";
         String title = "Title";
 
         //When
@@ -57,44 +54,55 @@ public class ChecklistServiceTest {
         ArgumentCaptor<Checklist> checklistArgumentCaptor = ArgumentCaptor.forClass(Checklist.class);
         Mockito.verify(listRepository, Mockito.times(1)).insert(checklistArgumentCaptor.capture());
         Checklist capturedChecklist = checklistArgumentCaptor.getValue();
+        assertThat(capturedChecklist.getTitle()).isEqualTo(expectedValue.getTitle()); 
         
-        
-
         Mockito.verify(mongoTemplate, Mockito.times(1)).findAndModify(
                 ArgumentMatchers.any(Query.class), 
                 ArgumentMatchers.any(Update.class), 
                 ArgumentMatchers.any(FindAndModifyOptions.class), 
                 ArgumentMatchers.any(Class.class));
-
-        
     }
 
     @Test
     @DisplayName("Should Replace Checklist's Old Title with New Title")
-    @Disabled
+    @SuppressWarnings("unchecked")
     public void modifyExistingChecklistTitle() {
+        String listId = "testId";
+        String title = "newTitle";
+        Checklist expectedValue = new Checklist("testId", "newTitle");
+
+        //Given 
+        Mockito.when(mongoTemplate.findAndModify(
+                    ArgumentMatchers.any(Query.class),
+                    ArgumentMatchers.any(Update.class),
+                    ArgumentMatchers.any(FindAndModifyOptions.class),
+                    ArgumentMatchers.any(Class.class))).thenReturn(expectedValue);
+        
+
+        //When
+        Checklist returnedValue = serviceUnderTest.modifyChecklist(listId, title);
+
+        //Then
+        assertThat(returnedValue.getTitle()).isEqualTo(expectedValue.getTitle());
 
     }
 
-    @Test
-    @DisplayName("Should Throw ApiRequestException When Modifying Non-existent Checklist")
-    @Disabled
-    public void throwExceptionNonexistentChecklist() {
+    // @Test
+    // @DisplayName("Should Throw ApiRequestException When Modifying Non-existent Checklist")
+    // public void throwExceptionNonexistentChecklist() {
+    //
+    // }
 
-    }
+    // @Test
+    // @DisplayName("Should Delete Existing Checklist")
+    // public void deleteExistingChecklist() {
+    //
+    // }
 
-    @Test
-    @DisplayName("Should Delete Existing Checklist")
-    @Disabled
-    public void deleteExistingChecklist() {
-
-    }
-
-    @Test
-    @DisplayName("Should Throw ApiRequestException When Deleting Non-existent Checklist")
-    @Disabled
-    public void throwExceptionDeletingChecklist() {
-
-    }
+    // @Test
+    // @DisplayName("Should Throw ApiRequestException When Deleting Non-existent Checklist")
+    // public void throwExceptionDeletingChecklist() {
+    //
+    // }
 
 }
