dbf16ad689dfa01eab0d8c577dfd2c6a0059a31e
victoresteban295 <esteban.victor295@gmail.com>
2023-06-06 16:23:18 -0700

    Finished Testing ChecklistController Class

--- src/test/java/com/academicdashboard/backend/checklist/ChecklistControllerTest.java
+++ src/test/java/com/academicdashboard/backend/checklist/ChecklistControllerTest.java
@@ -1,23 +1,18 @@
 package com.academicdashboard.backend.checklist;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.bson.types.ObjectId;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-
-@WebMvcTest(controllers = Checklist.class)
+@WebMvcTest(controllers = ChecklistController.class)
 public class ChecklistControllerTest {
 
     @MockBean
@@ -27,21 +22,45 @@ public class ChecklistControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("Should Return Newly Created Checklist When Making GET request to endpoints - /api/checklist/new/{userId} ")
+    @DisplayName("Should Return Newly Created Checklist When Making POST request to endpoints - /api/checklist/new/{userId}")
     public void shouldCreateNewChecklist() throws Exception {
-        //Mocked Response
-        Checklist checklist = new Checklist("listId", "title");
-        Checkpoint point01 = new Checkpoint("pointId01", "content01", false, false);
-        Checkpoint point02 = new Checkpoint("pointId02", "content02", false, false);
-        List<Checkpoint> points = new ArrayList<>();
-        points.add(point01);
-        points.add(point02);
-        checklist.setCheckpoints(points);
-        checklist.setId(new ObjectId());
+        Checklist checklist = new Checklist("12345", "Checklist Title");
 
-        Mockito.when(checklistService.createChecklist("userId", "title"))
+        Mockito.when(checklistService.createChecklist("ai49h5u84j98h3k2i5v2q", "Checklist Title"))
             .thenReturn(checklist);
 
-        mockMvc.perform(get("/api/checklist/new/userId"));
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/checklist/new/ai49h5u84j98h3k2i5v2q")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\": \"Checklist Title\"}"))
+            .andExpect(MockMvcResultMatchers.status().is(201))
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.listId", Matchers.is(checklist.getListId())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title", Matchers.is(checklist.getTitle())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.checkpoints", Matchers.is(checklist.getCheckpoints())));
+    }
+
+    @Test
+    @DisplayName("Should Return Modified Checklist When Making PUT request to endpoints - /api/checklist/modify/{listId}")
+    public void shouldModifyExistingChecklist() throws Exception {
+        Checklist response = new Checklist("12345", "New Checklist Title");
+
+        Mockito.when(checklistService.modifyChecklist("12345", "New Checklist Title"))
+            .thenReturn(response);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/checklist/modify/12345")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\": \"New Checklist Title\"}"))
+            .andExpect(MockMvcResultMatchers.status().is(200))
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.listId", Matchers.is(response.getListId())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title", Matchers.is(response.getTitle())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.checkpoints", Matchers.is(response.getCheckpoints())));
+    }
+
+    @Test
+    @DisplayName("Should Only Return 204 Status Code When Making DELETE request to endpoints - /api/checklist/delete/{listId}")
+    public void shouldDeleteExistingChecklist() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/checklist/delete/12345"))
+            .andExpect(MockMvcResultMatchers.status().is(204));
     }
 }
